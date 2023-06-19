1129cfc11a77449d8923f839456df6993b05f526
Yasha Sidin <124708836+yasha-sidin@users.noreply.github.com>
2023-06-19 14:42:15 +0300

    Create iCalculate.java

---
+++ src/main/java/iCalculate.java
@@ -0,0 +1,8 @@
+public interface iCalculate<T> {
+  
+  public T multiplication(T firstNum, T secondNum);
+
+  public T sum(T firstNum, T secondNum);
+
+  public T division(T firstNum, T secondNum);
+}
