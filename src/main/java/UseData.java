dc8026ca216d0ce2f78094c80574e2fad2b6d22d
Yasha Sidin <124708836+yasha-sidin@users.noreply.github.com>
2023-06-19 17:30:20 +0300

    Create UseData.java

---
+++ src/main/java/UseData.java
@@ -0,0 +1,30 @@
+public class UseData implements iPublisher {
+  Data data;
+
+  private Set<iObserver> observers = new HashSet<>();
+
+   @Override
+   public void registerObserver(iObserver observer) {
+       observers.add(observer);
+   }
+
+   @Override
+   public void unregisterObserver(iObserver observer) {
+       observers.remove(observer);
+   }
+
+   @Override
+   public void notifyObservers() {
+       for (iObserver observer : observers)
+           observer.update(data);
+   }
+
+   public Data getData() {
+       return data;
+   }
+
+   public void onNewData(Data newData) {
+       this.data = newData;
+       notifyObservers();
+   }
+}
