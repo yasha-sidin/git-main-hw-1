bb4410ac8d0c39433d88d6fb04648b84b9b51e80
Yasha Sidin <124708836+yasha-sidin@users.noreply.github.com>
2023-06-20 16:12:15 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) {
-
+        iPublisher logsPublisher = new UseData();
+        iGetModel firstFileLog = new LogToFile(logsPublisher, "FirstLogsFile.txt");
+        iGetModel secondFileLog = new LogToFile(logsPublisher, "SecondLogsFile.txt");
+        iGetModel listOfLogs = new LogToList(logsPublisher);
+        
+        ViewController program = new ViewController(); 
+        
+        program.initialize(logsPublisher);
     }
 }
